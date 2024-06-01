8c45eb122fa9daa3162c4cf5cde8cf8361c5afb9
Zijie Zhang <44576728+z-zijie@users.noreply.github.com>
2024-06-02 01:01:25 +0800

    test

--- analysis.py
+++ analysis.py
@@ -4,3 +4,8 @@ import pandas as pd
 if __name__ == "__main__":
     df = pd.read_csv('HotSearches.csv')
     tag_list = ['GPT', 'Bard', 'gpt']
+    print("WHJ")
+    print("WHJ")
+    print("WHJ")
+    print("WHJ")
+    print("WHJ")
